b043e459680646288ee75e20f3043490ace840e7
Lingbo Duan POPCORNA <paul120090105@yaho.com>
2024-06-06 18:27:34 -0400

    QRCode Size Vary

--- LabelQR/board_gds.py
+++ LabelQR/board_gds.py
@@ -2,10 +2,14 @@ import nazca as nd
 import nazca.geometries as geom
 from QRCode import encoderStandard, decoderStandard
 from QRCode import encoderMicro, decoderMicro
+
 '''
-Si grating: 1
+GDS
+Alignment: 0
+Si grating EBL: 1
 Si waveguide: 2
 SiN waveguide: 3
+SiN EBL: 4
 '''
 # ====================================Encoder Exmaple================================
 '''
@@ -14,9 +18,12 @@ The usage of encoder.makeLabelWithQRCode() is similar to nd.text()
 For details of parameters meanings please refer to ./QRCode/encoderStandard.py or ./QRCode/encoderMicro.py
 '''
 # encoderStandard.makeQRCode(text='D50S150WG6',height=0.2,align='cc',QRSize=0.8,layer=2).put(600)
-for i in range(-5000, 5000, 1000):
-    for j in range(-5000, 5000, 1000):
-        encoderStandard.makeQRCode(text='@({:d}, {:d})'.format(i,j),height=40,align='cc',layer=2).put(i, j)
+for i in range(-7000, 7000, 1000):
+    for j in range(-5000, 0000, 1000):
+        encoderStandard.makeQRCode(text='@({:d}, {:d})'.format(i,j),height=10*(((i+7000)/1000)%4+1),align='cc',layer=0).put(i, j)
+# 500 nm resolution
+# vary size
+
 
 # Bar Code
 
@@ -35,7 +42,7 @@ Use several different method to detect texts from QRCodes
 For details of parameters meanings please refer to ./QRCode/decoderStandard.py or ./QRCode/decoderMicro.py
 '''
 
-print('==============================exmaple 1==============================')
-print(decoderStandard.readQRCode("./QRCode/example_pos_1.png"))
-print('==============================exmaple 2==============================')
-print(decoderStandard.readQRCode("./QRCode/view_example.png"))
+# print('==============================exmaple 1==============================')
+# print(decoderStandard.readQRCode("./QRCode/example_pos_1.png"))
+# print('==============================exmaple 2==============================')
+# print(decoderStandard.readQRCode("./QRCode/view_example.png"))
